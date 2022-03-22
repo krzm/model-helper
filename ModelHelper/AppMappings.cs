4b92e2afb58f57c9292319a54f4bcf3dd39e79a4
krzm <kmazanek@gmail.com>
2022-03-22 19:49:04 +0100

    Add dependency sets

---
+++ ModelHelper/AppMappings.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using DIHelper.Unity;
+using Unity;
+
+namespace ModelHelper;
+
+public abstract class AppMappings<TMapperConfig>
+    : UnityDependencySet
+{
+    public AppMappings(
+        IUnityContainer container)
+        : base(container)
+    {
+    }
+
+    public override void Register() =>
+        Container.RegisterInstance(CreateMap().CreateMapper());
+
+    protected abstract MapperConfiguration CreateMap();
+}
---
+++ ModelHelper/AppValidators.cs
@@ -0,0 +1,21 @@
+﻿using DIHelper.Unity;
+using Unity;
+
+namespace ModelHelper;
+
+public class AppValidators 
+    : UnityDependencySet
+{
+    public AppValidators(
+        IUnityContainer container)
+        : base(container)
+    {
+    }
+
+    public override void Register()
+    {
+        RegisterArgumentModelValidators();
+    }
+
+    protected virtual void RegisterArgumentModelValidators() { }
+}
